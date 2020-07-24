debb836d1041148d3bd71b372a72b83b8ff64b74
surya568 <17pa1a0568@vishnu.edu.in>
2020-07-24 12:40:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,91 +1,58 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import Axios from 'axios';
 import "./App.css";
-import Status from "./components/Status";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board: Array(9).fill(null),
-      player: null,
-      winner: null,
-    };
-  }
-  handleClick(index) {
-    if (this.state.player && !this.state.winner) {
-      let newboard = this.state.board;
-      if (this.state.board[index] === null && this.state.winner === null) {
-        newboard[index] = this.state.player;
-        this.setState({
-          board: newboard,
-          player: this.state.player === "X" ? "0" : "X",
-        });
-      }
-    }
-    this.checkwinner();
-  }
-  checkwinner() {
-    let winLines = [
-      ["0", "1", "2"],
-      ["3", "4", "5"],
-      ["6", "7", "8"],
-      ["0", "3", "6"],
-      ["1", "4", "7"],
-      ["2", "5", "8"],
-      ["0", "4", "8"],
-      ["2", "4", "6"],
-    ];
-    for (let index = 0; index < winLines.length; index++) {
-      const [a, b, c] = winLines[index];
-      if (
-        this.state.board[a] &&
-        this.state.board[a] === this.state.board[b] &&
-        this.state.board[a] === this.state.board[c]
-      ) {
-        alert("WIN");
-        this.setState({
-          winner: this.state.board[a]
-        })
-      }
-    }
-  }
+import { v4 as uuidv4 } from "uuid";
+import Alert from "./components/Alert";
+import Recipe from "./components/recipe";
 
-  setPlayer(player) {
-    this.setState({
-      player: player,
-    });
-  }
-  resethandle() {
-    this.setState({
-      board: Array(9).fill(null),
-      player: null,
-      winner: null,
-    });
-  }
-  renderBoxes() {
-    return this.state.board.map((box, index) => (
-      <div className="box" key={index} onClick={() => this.handleClick(index)}>
-        {box}
-      </div>
-    ));
-  }
-  render() {
-    return (
-      <div className="container">
-        <h1 className="title">TIC TAC TOE</h1>
-        <Status
-          player={this.state.player}
-          winner={this.state.winner}
-          method={(e) => this.setPlayer(e)}
+function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+  const id = "d66a34e3";
+  const key = "3528fe5d0cbce80d41d32d7348577f12";
+  const url =`https://api.edamam.com/search?q=${query}&app_id=${id}&app_key=${key}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  const getData = async () => {
+    if (query !== " ") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      console.log("please fil the form");
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  };
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+  return (
+    <div className="APP">
+      <h1>Food searching app</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
         />
-        <div className="board">{this.renderBoxes()}</div>
-        <br />
-        <button className="reset" onClick={() => this.resethandle()}>
-          Reset
-        </button>
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => <Recipe key={uuidv4()} recipe={recipe} />)}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
 export default App;
