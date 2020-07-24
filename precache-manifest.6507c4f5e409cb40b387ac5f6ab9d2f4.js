4a3e336e2de16eb310a2332ac69b95250b5babe6
surya568 <17pa1a0568@vishnu.edu.in>
2020-07-24 12:43:52 +0530

    Updates

---
+++ precache-manifest.6507c4f5e409cb40b387ac5f6ab9d2f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2f1ea98309894d48720",
+    "url": "/recipe-react_app/static/css/main.f333860b.chunk.css"
+  },
+  {
+    "revision": "f2f1ea98309894d48720",
+    "url": "/recipe-react_app/static/js/main.0cf517ae.chunk.js"
+  },
+  {
+    "revision": "0b10a2cdbc9b47937b9e",
+    "url": "/recipe-react_app/static/js/runtime~main.d7c22ab5.js"
+  },
+  {
+    "revision": "9a296bce4199256b738f",
+    "url": "/recipe-react_app/static/js/2.d9111562.chunk.js"
+  },
+  {
+    "revision": "f5a7c11e8277eed6b576799ff4a3b4cd",
+    "url": "/recipe-react_app/index.html"
+  }
+];
